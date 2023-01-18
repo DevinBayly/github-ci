cc20b363d8297c842da5ad7b553592e9efcea36b
Devin Bayly <baylyd@email.arizona.edu>
2023-01-18 05:32:45 +0000

    Perhaps the first containerized step for ROCKE3D

---
+++ test_run_ROCKE3D.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cd $HOME/modelE2_planet_1.0/decks
+# this will probably show an error about a folder missing, but then it creates it
+make rundeck RUNSRC=E1oM20 RUN=E1oM20_Test
+# this downloads a bunch of files
+../exec/get_input_data -w E1oM20_Test $HOME/ModelE_Support/prod_input_files
+make clean; make -j setup RUN=E1oM20_Test
+echo "NOW STARTING THE FIRST 1HR RUN"
+../exec/runE E1oM20_Test -cold-restart -np 2
